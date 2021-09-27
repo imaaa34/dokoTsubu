198706ed7e60a2545a59b3baf33fb4edc41e0f48
imaaa34 <oct.imd34@gmail.com>
2021-09-27 12:27:27 +0900

    [Finish] chapter13

--- src/main/java/servlet/Main.java
+++ src/main/java/servlet/Main.java
@@ -1,11 +1,9 @@
 package servlet;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.GetMutterListLogic;
 import model.Mutter;
 import model.PostMutterLogic;
 import model.User;
@@ -22,14 +21,10 @@ public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ServletContext application = this.getServletContext();
-		List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
-		
-		if(mutterList == null) {
-			mutterList = new ArrayList<>();
-			application.setAttribute("mutterList", mutterList);
-		}
-		
+		GetMutterListLogic getMutterListLogic = new GetMutterListLogic();
+		List<Mutter> mutterList = getMutterListLogic.execute();
+		request.setAttribute("mutterList", mutterList);
+
 		HttpSession session = request.getSession();
 		User loginUser = (User) session.getAttribute("loginUser");
 		
@@ -46,21 +41,19 @@ public class Main extends HttpServlet {
 		String text = request.getParameter("text");
 		
 		if(text != null && text.length() != 0) {
-			ServletContext application = this.getServletContext();
-			List<Mutter> mutterList = (List<Mutter>) application.getAttribute("mutterList");
-			
 			HttpSession session = request.getSession();
-			User loginUser = (User) session.getAttribute("loginUser");
+			User loginUser = (User) session.getAttribute("loginUser");			
 			
 			Mutter mutter = new Mutter(loginUser.getName(), text);
 			PostMutterLogic postMutterLogic = new PostMutterLogic();
-			postMutterLogic.execute(mutter, mutterList);
-			
-			application.setAttribute("mutterList", mutterList);
+			postMutterLogic.execute(mutter);
 		} else {
 //			エラーメッセージを表示
 			request.setAttribute("errorMsg", "つぶやきが入力されていません");
 		}
+		GetMutterListLogic getMutterListLogic = new GetMutterListLogic();
+		List<Mutter> mutterList = getMutterListLogic.execute();
+		request.setAttribute("mutterList", mutterList);
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
 		dispatcher.forward(request, response);
